79e7ce651ebd4e7e7e3a8fc4564fe5119f8d2753
RazvanBarna <razvan_barna@yahoo.com>
2026-04-18 12:46:55 +0300

    pca eigenfaces, next : SVC

--- TestPy/Algs/HaarCascade_detection.py
+++ TestPy/Algs/HaarCascade_detection.py
@@ -3,47 +3,46 @@ from pathlib import Path
 import kagglehub
 import csv
 
+def haar_data(i:int):
+    path = kagglehub.dataset_download("iamtushara/face-detection-dataset")
+    dataset_path = Path(path) / "merged"
 
-path = kagglehub.dataset_download("iamtushara/face-detection-dataset")
-dataset_path = Path(path) / "merged"
+    images_train = dataset_path / "images" / "train"
+    images_validation = dataset_path / "images" / "validation"
+    labels_train = dataset_path / "labels" / "train"
 
-images_train = dataset_path / "images" / "train"
-images_validation = dataset_path / "images" / "validation"
-labels_train = dataset_path / "labels" / "train"
+    cascade_path = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    face_cascade = cv2.CascadeClassifier(cascade_path)
+    fieldsName = ['filename', 'c_min', 'c_max', 'r_min', 'r_max']
 
-cascade_path = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-face_cascade = cv2.CascadeClassifier(cascade_path)
-fieldsName = ['filename', 'c_min', 'c_max', 'r_min', 'r_max']
-i = 0
+    with open('../CSVs/haar_coords.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(fieldsName)
 
-with open('../CSVs/haar_coords.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(fieldsName)
+        idx = 0
+        for img_path in images_validation.iterdir():
+            if idx == i:
+                break
 
-    i = 0
-    for img_path in images_validation.iterdir():
-        if i == 100:
-            break
+            if img_path.suffix.lower() not in {".jpg", ".jpeg", ".png", ".bmp"}:
+                continue
 
-        if img_path.suffix.lower() not in {".jpg", ".jpeg", ".png", ".bmp"}:
-            continue
+            src = cv2.imread(str(img_path))
 
-        src = cv2.imread(str(img_path))
+            idx += 1
+            print(img_path.name)
 
-        i += 1
-        print(img_path.name)
+            gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
 
-        gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30)
-        )
+            for (x, y, w, h) in faces:
+                cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                row = [img_path.name, x, x + w, y, y + h]
+                writer.writerow(row)
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            row = [img_path.name, x, x + w, y, y + h]
-            writer.writerow(row)
-
-        cv2.imwrite(f"MyDataset/Haar_images/{img_path.name}", src)
+            cv2.imwrite(f"../MyDataset/Haar_images/{img_path.name}", src)
--- TestPy/Algs/Test_face_detection.py
+++ TestPy/Algs/Test_face_detection.py
@@ -2,20 +2,20 @@ import kagglehub
 from pathlib import Path
 import subprocess
 
-path = kagglehub.dataset_download("iamtushara/face-detection-dataset")
-dataset_path = Path(path) / "merged"
+def get_data(i : int):
+    path = kagglehub.dataset_download("iamtushara/face-detection-dataset")
+    dataset_path = Path(path) / "merged"
 
-images_train = dataset_path / "images" / "train"
-images_validation = dataset_path / "images" / "validation"
-labels_train = dataset_path / "labels" / "train"
+    images_train = dataset_path / "images" / "train"
+    images_validation = dataset_path / "images" / "validation"
+    labels_train = dataset_path / "labels" / "train"
 
-exe_path = "../exec/OpenCVApplication.exe"
-
-i = 0
-for img in images_validation.iterdir():
-    if i == 100:
-        break
-    if img.suffix.lower() in {".jpg", ".jpeg", ".png", ".bmp"}:
-        i = i+1
-        print(img.name)
-        subprocess.run([exe_path,str(img)])
+    idx = 0
+    exe_path = "../exec/OpenCVApplication.exe"
+    for img in images_validation.iterdir():
+        if idx == i:
+            break
+        if img.suffix.lower() in {".jpg", ".jpeg", ".png", ".bmp"}:
+            idx = idx+1
+            print(img.name)
+            subprocess.run([exe_path,str(img)])
